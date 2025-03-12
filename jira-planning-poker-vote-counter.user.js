c805f1ce5ed27d7108bb75d679456c95dfea226d
James Kriz <jim.kriz@kroger.com>
2025-03-12 07:48:13 -0400

    Adding a delay and retry for detecting voting panel

--- jira-planning-poker-vote-counter.user.js
+++ jira-planning-poker-vote-counter.user.js
@@ -5,36 +5,53 @@
 // @description  Add a total votes counter to the planning poker panel.
 // @author       jim@kriz.net
 // @match        https://jira.kroger.com/jira/browse/*
-// @run-at       document-end
+// @run-at       document-idle
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     const titleText = 'Easy Agile Planning Poker';
-    const planningPokerTitle = document.evaluate(
-        `//span[contains(text(), '${titleText}')]`,
-        document,
-        null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE,
-        null
-      ).singleNodeValue;    
-    const votesPanel = document.querySelector('[class*=PlanningPokerWebPanel__DisplayedVotesWrapper]');
 
-    if (!planningPokerTitle || !votesPanel) {
-        console.warn('Page may not be a jira issue page with a voting panel');
-        return;
+    if (!attachVoteCountListener()) {
+        // Try again after a delay, in case the elements are slow to render (seems like the votes panel is typically delayed a second or two)
+        setTimeout(() => {
+            attachVoteCountListener();
+        }, 2500);
     }
 
-    updateVoteCount();
+    function attachVoteCountListener() {
+        const { planningPokerTitle, votesPanel } = getVotingElements();
 
-    const observer = new MutationObserver(() => {
-        updateVoteCount();
-    });
-    observer.observe(votesPanel, { childList: true });
+        if (!planningPokerTitle || !votesPanel) {
+            console.warn('Page may not be a jira issue page with a voting panel');
+            return false;
+        }
 
-    function updateVoteCount() {
+        updateVoteCount(planningPokerTitle, votesPanel);
+
+        const observer = new MutationObserver(() => {
+            updateVoteCount(planningPokerTitle, votesPanel);
+        });
+        observer.observe(votesPanel, { childList: true });
+
+        return true;
+    }
+
+    function updateVoteCount(planningPokerTitle, votesPanel) {
         const totalVotes = votesPanel.children?.length || 0;
         planningPokerTitle.innerText = `${titleText} (Votes: ${totalVotes})`;
+    }
+
+    function getVotingElements() {
+        const planningPokerTitle = document.evaluate(
+            `//span[contains(text(), '${titleText}')]`,
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null
+          ).singleNodeValue;
+        const votesPanel = document.querySelector('[class*=PlanningPokerWebPanel__DisplayedVotesWrapper]');
+        return { planningPokerTitle, votesPanel };
     }
 })();
