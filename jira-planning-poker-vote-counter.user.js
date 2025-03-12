582a36a4e0e5cc02b13156d1dc8edb91c5488779
James Kriz <jim.kriz@kroger.com>
2025-03-12 11:12:53 -0400

    Added majority display

--- jira-planning-poker-vote-counter.user.js
+++ jira-planning-poker-vote-counter.user.js
@@ -33,14 +33,44 @@
         const observer = new MutationObserver(() => {
             updateVoteCount(planningPokerTitle, votesPanel);
         });
-        observer.observe(votesPanel, { childList: true });
+        observer.observe(votesPanel, { childList: true, subtree: true, attributes: true, characterData: true });
 
         return true;
     }
 
     function updateVoteCount(planningPokerTitle, votesPanel) {
         const totalVotes = votesPanel.children?.length || 0;
-        planningPokerTitle.innerText = `${titleText} (Votes: ${totalVotes})`;
+        const majorityVote = getMajorityVote(votesPanel);
+        planningPokerTitle.innerText = `${titleText} - Votes: ${totalVotes}${majorityVote ? ' - ' + majorityVote : ''}`;
+    }
+
+    function getMajorityVote(votesPanel) {
+        if (!votesPanel.children?.length) {
+            return null;
+        }
+        const votes = {};
+        [...votesPanel.children].forEach(vote => {
+            const voteEstimate = vote.querySelector('[class*=EstimateVote__EstimateShown]');
+            const voteValue = voteEstimate?.innerText;
+            if (voteValue) {
+                votes[voteValue] = votes[voteValue] ? votes[voteValue] + 1 : 1;
+            }
+        });
+        if (!Object.keys(votes).length) {
+            return null;
+        }
+
+        const majorityVote = Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b);
+        const majorityVoteCount = votes[majorityVote];
+        const majorityVotePercent = (majorityVoteCount / votesPanel.children.length) * 100;
+        const tieVotes = Object.keys(votes).filter(vote => votes[vote] === majorityVoteCount);
+        const isTie = tieVotes.length > 1;
+
+        if (isTie) {
+            return `Majority: ${tieVotes.join(', ')} (${majorityVoteCount} votes each, ${majorityVotePercent.toFixed(0)}%)`;
+        } else {
+            return `Majority: ${majorityVote} (${majorityVoteCount} votes, ${majorityVotePercent.toFixed(0)}%)`;
+        }
     }
 
     function getVotingElements() {
